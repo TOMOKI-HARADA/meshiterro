ed754f0d3a9d1bba5b25d9b44ecbc29ef30f7d4b
TOMOKI-HARADA <aceblack702@gmail.com>
2021-02-24 13:17:05 +0000

    start5-4

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '189acc9f386db26444608e5753d4eb4966d7853bd90e1fa9dd33d55fc3a4bafb211287a56fff643c14c7059833e6d1fbea5486a298e1a7ead69cc78ac4f8c108'
